c99dc87d4930fdf026a743ca2b6b9711dc2572d7
Piotr Murach <pmurach@gmail.com>
2012-12-02 21:32:11 +0000

    Add library files.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'rubygems'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rspec'
+require 'equatable'
+
+RSpec.configure do |config|
+  config.order = :rand
+end
